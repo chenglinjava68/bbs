57232fba9c3b005dcc2d4ee02df6378ad4a72943
e <e>
2018-06-08 21:42:38 +0800

    update 2.0

--- bbs-admin/src/main/java/com/quark/admin/AdminApplication.java
+++ bbs-admin/src/main/java/com/quark/admin/AdminApplication.java
@@ -21,7 +21,7 @@ public class AdminApplication {
 
     @Bean
     public EmbeddedServletContainerCustomizer containerCustomizer() {
-
+            ///dada
         return (container -> {
             ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
             ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
